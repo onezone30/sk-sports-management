3f310913458fe2b317a2ba448780601eb980bd41
Cesar Pinero <cpinero522003@gmail.com>
2026-02-21 00:40:02 +0800

    auth provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "./providers/AuthProvider";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AuthProvider>
+        <BrowserRouter>
+            <AppRoutes />
+        </BrowserRouter>
+    </AuthProvider> 
   );
 }
